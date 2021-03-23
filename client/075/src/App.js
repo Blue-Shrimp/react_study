111eed8ff3b619506f28bc5582161155a9fa5dc9
DESKTOP-D0DOIQA\IT학교 <wkdtmd1202@naver.com>
2021-03-23 17:12:35 +0900

    085까지 실행

---
+++ client/075/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactHoc
+from './Hoc/R075_ReactHoc'
+function App() {
+  return (
+    <div>
+      <h1>Start React 200!</h1>
+      <ReactHoc name='React200'/>
+    </div>
+  );
+}export default App;
